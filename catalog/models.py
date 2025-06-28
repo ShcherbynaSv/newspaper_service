277cfd628b5935259fcc1ae2e0b4cfc1588b0473
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-06-28 11:06:10 +0300

    Add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper_service import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ("username",)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField()
+    topics = models.ManyToManyField(Topic, related_name="articles")
+    redactors = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="articles"
+    )
