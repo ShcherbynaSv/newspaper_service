1d4eae6c0a1fb7db094b50f543524a926a80461e
Svitlana Shcherbyna <sw.shcherbina@gmail.com>
2025-07-18 15:50:25 +0300

    Create templates folder with base.html, sidebar.html, index.html files in catalog and includes folders. Change index view to work with templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Topic, Redactor, Article
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h1>Newspaper Catalog</h1>")
+    context = {
+        "topics": Topic.objects.all().count(),
+        "redactors": Redactor.objects.all().count(),
+        "articles": Article.objects.all().count(),
+    }
+    return render(request, "catalog/index.html", context=context)
